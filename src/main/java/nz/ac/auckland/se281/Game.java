9ec4310e9a7e0463689299738f226c36666f687f
oorjagandhi <oorjagandhi@gmail.com>
2024-05-11 14:25:21 +1200

    Implemented the play function in the code

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -5,13 +5,26 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
+  private int roundNumber;
+  private String playerName;
+  private Difficulty difficulty;
+  private Choice playerChoice;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
+    this.playerName = options[0];
+    this.difficulty = difficulty;
+    this.playerChoice = choice;
+    this.roundNumber = 0;
+
+    // Welcome the player
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
-  public void play() {}
+  public void play() {
+    this.roundNumber++;
+    MessageCli.START_ROUND.printMessage(String.valueOf(this.roundNumber));
+  }
 
   public void endGame() {}
 
