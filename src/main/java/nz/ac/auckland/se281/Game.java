156d69f6b3fc25d5355c2aa0d190cdedd07f36d8
oorjagandhi <oorjagandhi@gmail.com>
2024-05-12 22:55:04 +1200

    Added logic to prevent game from playing unless a game has started

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -9,6 +9,7 @@ public class Game {
   private String playerName;
   private Choice playerChoice;
   private HAL9000 hal9000;
+  private boolean gameHasStarted;
 
   /**
    * The new game method is called to start a new game of Odds and Evens.
@@ -24,6 +25,8 @@ public class Game {
     this.playerChoice = choice;
     this.playerName = options[0];
 
+    gameHasStarted = true;
+
     // Create the AI based on the difficulty and choice
     this.hal9000 = AIFactory.createAi(difficulty, choice);
     this.hal9000.reset();
@@ -34,30 +37,34 @@ public class Game {
 
   /** Play a round of the game. */
   public void play() {
-    this.roundNumber++;
-    MessageCli.START_ROUND.printMessage(String.valueOf(this.roundNumber));
+    if (gameHasStarted) {
+      this.roundNumber++;
+      MessageCli.START_ROUND.printMessage(String.valueOf(this.roundNumber));
 
-    Player player = new Player();
+      Player player = new Player();
 
-    int playerAction = player.play();
-    int hal9000Action = hal9000.play();
+      int playerAction = player.play();
+      int hal9000Action = hal9000.play();
 
-    MessageCli.PRINT_INFO_HAND.printMessage(playerName, Integer.toString(playerAction));
-    MessageCli.PRINT_INFO_HAND.printMessage("HAL-9000", Integer.toString(hal9000Action));
+      MessageCli.PRINT_INFO_HAND.printMessage(playerName, Integer.toString(playerAction));
+      MessageCli.PRINT_INFO_HAND.printMessage("HAL-9000", Integer.toString(hal9000Action));
 
-    // Determine the winner
-    int sum = playerAction + hal9000Action;
-    Choice sumOddOrEven = (sum % 2 == 0) ? Choice.EVEN : Choice.ODD;
+      // Determine the winner
+      int sum = playerAction + hal9000Action;
+      Choice sumOddOrEven = (sum % 2 == 0) ? Choice.EVEN : Choice.ODD;
 
-    boolean playerWins = (playerChoice == sumOddOrEven);
-    hal9000.updateCounts(playerAction, !playerWins);
+      boolean playerWins = (playerChoice == sumOddOrEven);
+      hal9000.updateCounts(playerAction, !playerWins);
 
-    if (playerChoice == sumOddOrEven) {
-      MessageCli.PRINT_OUTCOME_ROUND.printMessage(
-          Integer.toString(sum), sumOddOrEven.name(), playerName);
+      if (playerChoice == sumOddOrEven) {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(
+            Integer.toString(sum), sumOddOrEven.name(), playerName);
+      } else {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(
+            Integer.toString(sum), sumOddOrEven.name(), "HAL-9000");
+      }
     } else {
-      MessageCli.PRINT_OUTCOME_ROUND.printMessage(
-          Integer.toString(sum), sumOddOrEven.name(), "HAL-9000");
+      MessageCli.GAME_NOT_STARTED.printMessage();
     }
   }
 
